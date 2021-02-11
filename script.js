78ce73b6d52d15b991dd02a9e6d53a83a15cf1fd
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-11 17:37:29 +0000

    edc

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.getElementById("gold").addEventListener("click",button2)
+document.getElementById('gold').addEventListener('click', button2)
 
-document.getElementById("gray").addEventListener("click",button3)
+document.getElementById('gray').addEventListener('click', button3)
 
 function button2 () {
-  document.getElementById("paragraph1").innerHTML = "Hello world"
-  document.getElementById("background").style.backgroundColor ="gray"
+  document.getElementById('paragraph1').innerHTML = 'Hello world'
+  document.getElementById('background').style.backgroundColor = 'gray'
 }
 
 function button3 () {
-document.getElementById("background").style.backgroundColor = "gold"
+  document.getElementById('background').style.backgroundColor = 'gold'
 }
